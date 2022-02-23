9b95028fed0470c27e696ecb7bbf5bf3ebcd61e9
m-ghiringhelli <mghiring@gmail.com>
2022-02-23 15:46:01 -0800

    on load displays movie info with renderMovie

--- reviews/reviews.js
+++ reviews/reviews.js
@@ -1,10 +1,19 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { checkAuth, logout, fetchMovieById } from '../fetch-utils.js';
+import { renderMovie } from '../render-utils.js';
 
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
+const body = document.getElementById('main-body');
 
 logoutButton.addEventListener('click', () => {
     logout();
+});
+
+window.addEventListener('load', async () => {
+    const params = new URLSearchParams(window.location.search);
+    const id = +params.get('id');
+    const movie = await fetchMovieById(id);
+    body.append(renderMovie(movie));
 });
 
