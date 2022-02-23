04b4a7ec41e3976794a5ab9e0a48fb0496c7d86f
Kamie Short <kamie.short@gmail.com>
2022-02-23 15:02:09 -0800

    added logout to each page

---
+++ reviews/reviews.js
@@ -0,0 +1,10 @@
+import { checkAuth, logout } from '../fetch-utils.js';
+
+checkAuth();
+
+const logoutButton = document.getElementById('logout');
+
+logoutButton.addEventListener('click', () => {
+    logout();
+});
+
