3135f518698fb7adda0f726868d6e02ed586cb42
m-ghiringhelli <mghiring@gmail.com>
2022-02-23 15:08:43 -0800

    fixed spelling error

--- test/utils.test.js
+++ test/utils.test.js
@@ -4,7 +4,7 @@ const test = QUnit.test;
 const movie = {
     created_at: '2022-02-23T21:52:01+00:00',
     description: "A pragmatic paleontologist touring an almost complete theme park on an island in Central America is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose.",
-    director: 'Stephen Spielberg',
+    director: 'Stevgen Spielberg',
     genre: 'Sci-Fi',
     id: 7,
     image: '../assets/jurassicpark.jpg',
@@ -30,7 +30,7 @@ test('time to test a function', (expect) => {
 });
 
 test('should take in movie object and render', (expect) => {
-    const expected = `<main id="main-review"><div id="movie-detail-container"><img src="../assets/jurassicpark.jpg" class="detail-image"><p class="detail-title">JURASSIC PARK</p><p class="detail-year">1993</p><p class="detail-genre">Sci-Fi</p><p class="detail-director">Stephen Spielberg</p><p class="detail-description">A pragmatic paleontologist touring an almost complete theme park on an island in Central America is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose.</p></div><form id="add-review-form"><textarea class="add-review"></textarea><select class="add-rating"><option value="">add your rating</option><option value="1">*</option><option value="2">**</option><option value="3">***</option><option value="4">****</option><option value="5">*****</option></select><input type="submit" class="submit-review-button"></form></main>`;
+    const expected = `<main id="main-review"><div id="movie-detail-container"><img src="../assets/jurassicpark.jpg" class="detail-image"><p class="detail-title">JURASSIC PARK</p><p class="detail-year">1993</p><p class="detail-genre">Sci-Fi</p><p class="detail-director">Steven Spielberg</p><p class="detail-description">A pragmatic paleontologist touring an almost complete theme park on an island in Central America is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose.</p></div><form id="add-review-form"><textarea class="add-review"></textarea><select class="add-rating"><option value="">add your rating</option><option value="1">*</option><option value="2">**</option><option value="3">***</option><option value="4">****</option><option value="5">*****</option></select><input type="submit" class="submit-review-button"></form></main>`;
 
     const actual = renderMovie(movie);
 
