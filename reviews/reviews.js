57602dfa33a8a4a28b5aec3393bd1a16e8930bd5
m-ghiringhelli <mghiring@gmail.com>
2022-02-23 16:31:48 -0800

    created div to hold rendered movie

--- reviews/reviews.js
+++ reviews/reviews.js
@@ -14,7 +14,6 @@ window.addEventListener('load', async () => {
     const params = new URLSearchParams(window.location.search);
     const id = +params.get('id');
     const movie = await fetchMovieById(id);
-    console.log(movie);
     body.append(renderMovie(movie));
 });
 
